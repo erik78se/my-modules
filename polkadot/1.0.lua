4f29b037bc6184bdc1b38819527c281d88f49f10
erik78se <erik.lonroth@gmail.com>
2021-09-19 08:33:55 +0000

    initial

---
+++ polkadot/1.0.lua
@@ -0,0 +1,13 @@
+help([[
+For detailed instructions, go to:
+   https://polkadot.network
+
+]])
+whatis("Version: 5.0.1")
+whatis("Keywords: System, Utility")
+whatis("URL: http://polkadot.js.org")
+whatis("Description: Parallel, graphical, symbolic debugger")
+
+setenv(       "DDTPATH",        "/opt/apps/ddt/5.0.1/bin")
+prepend_path( "PATH",           "/opt/apps/ddt/5.0.1/bin")
+prepend_path( "LD_LIBRARY_PATH","/opt/apps/ddt/5.0.1/lib")
